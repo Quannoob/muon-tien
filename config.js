0e58409499d90da7f894a939dbfcdd2879e709a8
Quannoob <86725550+Quannoob@users.noreply.github.com>
2021-06-30 20:41:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,2 @@
-const CONFIG = {
-    introTitle: 'Chào anh Long !',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ hết tiền xài
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là cho mượn 5 chục `,
-    btnIntro: 'hihi',
-    title: 'Cho mượn 5 chục đi Long ơi 😙',
-    desc: 'Cứu mạng ! ',
-    btnYes: 'Ok mai qua tao lấy <33',
-    btnNo: 'Không, cút dùm :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao chỉ có mình a cho e mượn tiền <3',
-    btnReply: 'Gửi cho Quân <3',
-    reply: 'Tại vì tao dơ tiền được chưa khỏi cần trả lại cho tao <33333333',
-    mess: 'Em biết mà 🥰. Yêu Long nhiều nhiều 😘😘',
-    messDesc: 'Moaaaaa.',
-    btnAccept: 'Okiiiii lun. <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+
 }
